aeb15325be7c4a1e85f1109f8470ae02ddd18514
facuevas <franciscuevas4@gmail.com>
2021-03-12 23:03:47 -0800

    Completed Day 40

---
+++ Day 40 - Background Boxes/script.js
@@ -0,0 +1,19 @@
+const boxes = document.getElementById("boxes");
+const magicButton = document.getElementById("btn");
+
+magicButton.addEventListener("click", () => {
+  boxes.classList.toggle("big");
+});
+
+function createBoxes() {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      const box = document.createElement("div");
+      box.classList.add("box");
+      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
+      boxes.append(box);
+    }
+  }
+}
+
+createBoxes();
